9b8b83048599385c8033f42b55d2962d7c7185df
lemavisaitov <visaitovlema@gmail.com>
2024-12-20 22:54:15 +0300

    added logging, .gitignore and Makefile

---
+++ pkg/logging/logging.go
@@ -0,0 +1,76 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type writerHook struct {
+	Writers   []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, w := range hook.Writers {
+		_, err = w.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) GetLoggerWithField(key string, value interface{}) *Logger {
+	return &Logger{l.WithField(key, value)}
+}
+
+func init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+		FullTimestamp: true,
+	}
+	err := os.Mkdir("logs", 0755)
+	if err != nil && os.IsNotExist(err) {
+		panic(err)
+	}
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil && os.IsNotExist(err) {
+		panic(err)
+	}
+
+	l.SetOutput(io.Discard)
+	l.AddHook(&writerHook{
+		Writers:   []io.Writer{allFile},
+		LogLevels: logrus.AllLevels,
+	})
+
+	e = logrus.NewEntry(l)
+}
